5a0b3cd0ea4cea2cac56fdef884669b1b098f47c
Yusuke Takahashi <youthkee@gmail.com>
2016-02-13 10:18:48 +0900

    Add vccc setting files

---
+++ provision-post.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+#/usr/local/bin/wp --path=/var/www/wordpress db import /vagrant/import.sql
+/usr/local/bin/wp --path=/var/www/wordpress plugin install wordpress-importer --activate
