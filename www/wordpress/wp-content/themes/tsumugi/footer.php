98182805e16c59c5c600a0ff1169b6cd107cd47a
Yusuke Takahashi <youthkee@gmail.com>
2016-02-13 10:32:28 +0900

    Add underscores base

---
+++ www/wordpress/wp-content/themes/tsumugi/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tsumugi
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tsumugi' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'tsumugi' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'tsumugi' ), 'tsumugi', '<a href="http://littlebird.mobi" rel="designer">youthkee</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
