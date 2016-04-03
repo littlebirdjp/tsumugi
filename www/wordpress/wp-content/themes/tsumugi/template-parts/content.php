4ce83d5ba817950e6116293a5617cb7bb86424fa
Yusuke Takahashi <youthkee@gmail.com>
2016-04-03 23:53:12 +0900

    Add navigation icons

--- www/wordpress/wp-content/themes/tsumugi/template-parts/content.php
+++ www/wordpress/wp-content/themes/tsumugi/template-parts/content.php
@@ -30,7 +30,7 @@
 		<?php
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'tsumugi' ), array( 'span' => array( 'class' => array() ) ) ),
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&#x03E;</span>', 'tsumugi' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 
