05f09945670d2f88c9c1c62df8c3e02a4e48d65c
Yusuke Takahashi <youthkee@gmail.com>
2016-05-01 07:53:56 +0900

    Fix a typo

--- www/wordpress/wp-content/themes/tsumugi/functions.php
+++ www/wordpress/wp-content/themes/tsumugi/functions.php
@@ -117,7 +117,7 @@ function tsumugi_scripts() {
 	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css', array(), '4.0.0-alpha.2', 'all' );
 	wp_enqueue_style( 'underscores-style', get_stylesheet_uri(), array('bootstrap-style'), '1.0.0', 'all' );
 	wp_enqueue_style( 'tsumugi-style', get_template_directory_uri() . '/bootstrap/tsumugi.css', array('underscores-style'), '1.0.0', 'all' );
-l:
+
 	wp_enqueue_style( 'google-font', 'http://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope|Source+Sans+Pro:300' );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
 
