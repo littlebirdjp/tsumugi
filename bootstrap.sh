3b3abd855509513f00c2e8383f4c61c8fff880cd
Yusuke Takahashi <youthkee@gmail.com>
2016-05-03 12:32:46 +0900

    Change update commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,5 @@
 
 cp bower_components/bootstrap/scss/*.scss sass/bootstrap/
 cp bower_components/bootstrap/scss/mixins/*.scss sass/bootstrap/mixins/
+rm -rf bower_components/bootstrap/scss
+rm -rf bower_components/jquery
