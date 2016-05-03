3d9f4e16f3300613e3357b523cdd9dab894b6869
Yusuke Takahashi <youthkee@gmail.com>
2016-05-03 10:36:36 +0900

    Delete vccw files

--- provision-post.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-#/usr/local/bin/wp --path=/var/www/wordpress db import /vagrant/import.sql
-/usr/local/bin/wp --path=/var/www/wordpress plugin install wordpress-importer --activate
