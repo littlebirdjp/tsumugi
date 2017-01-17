04289300d8a0ff129a4f0bf251f10e15d53c972a
Yusuke Takahashi <youthkee@gmail.com>
2017-01-18 08:05:40 +0900

    Add setting files

---
+++ docs/_work/bin/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp bower_components/bootstrap/dist/css/*.css ../css/
+cp bower_components/bootstrap/dist/js/*.js ../js/
+cp bower_components/jquery/dist/*.js ../js/
+cp bower_components/tether/dist/js/*.js ../js/
---
+++ docs/_work/gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var prettify = require('gulp-prettify');
+var browserSync = require('browser-sync');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var PostcssSimpleVars = require('postcss-simple-vars');
+var PostcssNested = require('postcss-nested');
+var PostcssMixins = require('postcss-mixins');
+var PostcssImport = require('postcss-import');
+var csscomb = require('gulp-csscomb');
+
+var paths = {
+  'src': 'src/',
+  'dist': '../'
+}
+
+gulp.task('bs', function() {
+  browserSync.init({
+    server: {
+      baseDir: paths.dist,
+      index: 'index.html'
+    },
+    notify: true
+  });
+});
+
+gulp.task('html', function() {
+  return gulp.src([
+    paths.src + '**/*.pug',
+    '!' + paths.src + '**/_*.pug'
+    ])
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('prettify', ['html'], function() {
+  return gulp.src([
+    paths.dist + '**/*.html',
+    '!' + paths.dist +'_work/**/*.html'
+    ])
+    .pipe(prettify({
+      brace_style: 'collapse',
+      indent_size: 2,
+      indent_char: ' '
+    }))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('css', function() {
+  var processors = [
+      cssnext({browsers: ['last 2 version']}),
+      PostcssMixins(),
+      PostcssSimpleVars(),
+      PostcssNested(),
+      PostcssImport(),
+  ];
+  return gulp.src([
+    paths.src + '**/*.css',
+    '!' + paths.src + '**/_*.css'
+    ])
+    .pipe(postcss(processors))
+    .pipe(csscomb())
+    .pipe(gulp.dest(paths.dist))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch([paths.src + '**/*.pug'], ['prettify']);
+  gulp.watch([paths.src + '**/*.css'], ['css']);
+});
+
+gulp.task('default', ['bs', 'prettify', 'css', 'watch']);
