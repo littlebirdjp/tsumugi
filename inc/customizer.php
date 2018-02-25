15633e1e7e8a3fb3ef706d31bb0584e93cbf6cf1
Yusuke Takahashi <youthkee@gmail.com>
2018-02-25 13:25:32 +0900

    Add selective refresh supports

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,6 +14,14 @@ function tsumugi_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->selective_refresh->add_partial( 'blogname', array(
+    'selector' => '.site-title a',
+    'render_callback' => 'tsumugi_customize_partial_blogname',
+	) );
+	$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+    'selector' => '.site-description',
+    'render_callback' => 'tsumugi_customize_partial_blogdescription',
+	) );
 }
 add_action( 'customize_register', 'tsumugi_customize_register' );
 
