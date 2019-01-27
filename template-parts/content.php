b46c032902ed23e71d3365efc94ddcd77179cf86
Yusuke Takahashi <youthkee@gmail.com>
2019-01-27 10:11:02 +0900

    Add and Fix block styles

--- template-parts/content.php
+++ template-parts/content.php
@@ -36,7 +36,7 @@
 		<?php
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&gt;</span>', 'tsumugi' ), array( 'span' => array( 'class' => array() ) ) ),
+				wp_kses( __( 'Continue reading %s', 'tsumugi' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 
